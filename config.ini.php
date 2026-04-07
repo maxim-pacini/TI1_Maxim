76c8112ea7a8db2414e6212c30f042935f61b39a
maxim-pacini <maxim10pacini@gmail.com>
2026-04-07 10:24:23 +0200

    Configuration du fichier config.ini et de index.php dans le dossier public

---
+++ config.ini.php
@@ -0,0 +1,7 @@
+<?php
+
+// on déclare une constante qui contient un chemin dynamique
+const CHEMIN = __dir__;
+
+// on déclare un tableau qui contient les valeurs de nos pages
+const PAGES =['accueil','geographie','histoire','culture','galerie','contact','liens'];
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+// on vient chargé les dépendances
+require_once = "../config.php"
+
+if(!isset($_GET['p'])){
+
+    include CHEMIN."/view/accueil.php";
+
+}elseif(in_array($_GET['p'],PAGES)){
+
+    include CHEMIN."/view/".$_GET['p'].".php";
+
+}else{
+    include CHEMIN."/view/erreur404.php"
+}
